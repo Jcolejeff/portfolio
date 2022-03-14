693b760962026609b008e8d0814687652bda4c97
Jcolejeff <62331630+Jcolejeff@users.noreply.github.com>
2022-03-14 12:06:11 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -5,9 +5,12 @@ const Projects = [
     title: "TRIVIA APP",
     dates: "2022",
     aboutProject: [
-      "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
-      "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
-      "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
+      " Purpose and Goal:I wanted to learn how to work with APIs and handling forms in react , and this was the perfect project",
+      "General Requirement: The requirement I had in mind is that the website must be fast and responsive, and users should be able to see their total score at the end of the session ",
+      "Stack Explanation:  It's mainly built on react, some normal css and styled components too,mainly because it lets you write actual CSS and it's popular. I used react reveal library to the animation,OPENTRIVADB APIs docs is great and easy to understand
+",
+      "Challenges: settings up the urls parameters were bit of a hassle, because of the categories, also randomising answers was quite interesting, i had to do lots of googling"
+     
     ],
     project: "trivia app,",
     live: "https://trivia-plus.netlify.app/",
